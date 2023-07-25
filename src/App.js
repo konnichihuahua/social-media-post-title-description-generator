edd5d6f05140a73a31b5a0e05fba3a4515a1318d
Josh Sargento <josh-sargento@Joshs-MacBook-Air.local>
2023-07-26 02:05:31 +0800

    finished setting up local server

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import SocialMediaForm from "./components/SocialMediaForm";
 import Results from "./components/Results";
-import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 
 function App() {
   const [title, setTitle] = useState("");
   const [caption, setCaption] = useState("");
-
-  const configuration = new Configuration({
-    apiKey: process.env.REACT_APP_API_URL,
-  });
-  delete configuration.baseOptions.headers["User-Agent"];
-
-  const openai = new OpenAIApi(configuration);
 
   const onSubmit = (data) => {
     getCaption(data);
@@ -22,38 +14,14 @@ function App() {
   };
 
   const getTitle = (data) => {
-    openai
-      .createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: `Write a youtube shorts title about ${data}. Make it only 5 words.`,
-          },
-        ],
-      })
-      .then((result) => {
-        console.log(result.data.choices[0].message.content);
-        const newTitle = result.data.choices[0].message.content;
-        setTitle(newTitle);
-      });
+    fetch(`http://localhost:5000/get/title/${data}`)
+      .then((response) => response.json())
+      .then((data) => setTitle(data.title));
   };
   const getCaption = (data) => {
-    openai
-      .createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: `Write a 1 sentence tiktok caption about ${data}. Write like a native english speaker. Then add relevant hashtags. Hashtags must be in lowercase. Add the hashtag #degreefree, #college, #collegetips, #jobs, #jobsearch, #jobhunt, #jobhunting.`,
-          },
-        ],
-      })
-      .then((result) => {
-        console.log(result.data.choices[0].message.content);
-        const newCaption = result.data.choices[0].message.content;
-        setCaption(newCaption);
-      });
+    fetch(`http://localhost:5000/get/description/${data}`)
+      .then((response) => response.json())
+      .then((data) => setCaption(data.description));
   };
   return (
     <div className="App flex flex-col justify-center align-center p-5 ">
